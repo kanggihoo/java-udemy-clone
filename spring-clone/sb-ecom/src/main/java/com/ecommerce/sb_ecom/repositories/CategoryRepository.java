aa3181e32691d3149e1e25b8af64914774674d5e
kkh <11kkh19999.gmail.com>
2026-01-21 00:15:42 +0900

    service 계층내에서 repository와의 연결간의 에러 1차 수정 및 h2 db연결 확인 필요 in HOME

--- spring-clone/sb-ecom/src/main/java/com/ecommerce/sb_ecom/repositories/CategoryRepository.java
+++ spring-clone/sb-ecom/src/main/java/com/ecommerce/sb_ecom/repositories/CategoryRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CategoryRepositoriy extends JpaRepository<CategoryEntity, Long> {
+public interface CategoryRepository extends JpaRepository<CategoryEntity, Long> {
     CategoryEntity findByCategoryName(String categoryName);
 }
--- spring-clone/sb-ecom/src/main/java/com/ecommerce/sb_ecom/service/CategoryServiceImpl.java
+++ spring-clone/sb-ecom/src/main/java/com/ecommerce/sb_ecom/service/CategoryServiceImpl.java
@@ -1,57 +1,56 @@
 package com.ecommerce.sb_ecom.service;
 
+import com.ecommerce.sb_ecom.dto.Category;
+import com.ecommerce.sb_ecom.entity.CategoryEntity;
+import com.ecommerce.sb_ecom.repositories.CategoryRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import com.ecommerce.sb_ecom.entity.CategoryEntity;
-import com.ecommerce.sb_ecom.repositories.CategoryRepositoriy;
-import java.util.*;
 import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CategoryServiceImpl implements CategoryService {
 
-    private final CategoryRepositoriy categoryRepositoriy;
+    private final CategoryRepository categoryRepository;
 
-    public CategoryServiceImpl(CategoryRepositoriy categoryRepositoriy) {
-        this.categoryRepositoriy = categoryRepositoriy;
+    public CategoryServiceImpl(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
     }
 
     @Override
-    public void createCategory(CategoryEntity category) {
+    public List<Category> getAllCategories() {
+        List<CategoryEntity> categoryEntities = categoryRepository.findAll();
+        return categoryEntities.stream()
+                .map(entity -> new Category(entity.getCategoryId(), entity.getCategoryName()))
+                .collect(Collectors.toList());
+    }
 
-        category.setCategoryId(nextId++);
-        categories.add(category);
+    @Override
+    public void createCategory(Category category) {
+        CategoryEntity categoryEntity = new CategoryEntity();
+        categoryEntity.setCategoryName(category.getCategoryName());
+        categoryRepository.save(categoryEntity);
     }
 
     @Override
     public String deleteCategory(Long categoryId) {
+        CategoryEntity categoryEntity = categoryRepository.findById(categoryId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Category not found"));
 
-        CategoryRepositoriy category = categories.stream().filter(c -> c.getCategoryId().equals(categoryId)).findFirst()
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Resource not found"));
-
-        categories.remove(category);
+        categoryRepository.delete(categoryEntity);
         return "categoryId: " + categoryId + " deleted successfully !!";
     }
 
     @Override
-    public List<CategoryRepositoriy> getAllCategories() {
-        return this.categoryRepositoriy.findAll()
+    public Category updateCategory(Category category, Long categoryId) {
+        CategoryEntity existingCategory = categoryRepository.findById(categoryId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Category not found"));
+
+        existingCategory.setCategoryName(category.getCategoryName());
+        CategoryEntity savedCategory = categoryRepository.save(existingCategory);
+
+        return new Category(savedCategory.getCategoryId(), savedCategory.getCategoryName());
     }
-
-    @Override
-    public CategoryRepositoriy updateCategory(CategoryRepositoriy category, Long categoryId) {
-
-        Optional<CategoryRepositoriy> optionalCategory = categories.stream()
-                .filter(c -> c.getCategoryId().equals(categoryId))
-                .findFirst();
-        if (optionalCategory.isPresent()) {
-            CategoryRepositoriy existingCategory = optionalCategory.get();
-            existingCategory.setCategoryName(category.getCategoryName());
-            return existingCategory;
-        } else {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Category not found");
-        }
-
-    }
-
 }
